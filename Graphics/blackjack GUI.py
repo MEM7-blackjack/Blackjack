e8529e35e0c1436f77d749790fef38c5fc4e9c37
Maylin Martinez <mmartinez@conncoll.edu>
2019-10-30 17:55:37 -0400

    Graphics

---
+++ Graphics/blackjack GUI.py
@@ -0,0 +1,24 @@
+#BlackJack GUI
+
+from graphics import *
+from button import *
+
+
+def graphic():
+    win = GraphWin("Blackjack!", 800,500)
+    background_image = Image(Point(400,200),"table.png")
+    background_image.draw(win)
+
+
+    hit = Button(win, Point(300,300),60,30,"HIT")
+    stand = Button(win, Point(500,300),60,30,"STAND")
+    end = Button(win, Point(100,300),60,30,"EXIT")
+    reset = Button(win, Point(400,400),60,30,"RESET")
+
+    hit.activate()
+    stand.activate()
+    end.activate()
+    reset.activate()
+    
+graphic()
+ 
