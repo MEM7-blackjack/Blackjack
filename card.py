95d10ef3ae0e3109adefae6af5f4f2a4578961f1
asd417 <loctitematt417@gmail.com>
2019-10-30 17:58:36 -0400

    Added getSide and setSide for flipping cards.

--- card.py
+++ card.py
@@ -2,8 +2,10 @@ class Card:
     def __init__(self,id): #id is card image name
         self.num = id[1:]
         self.id = id
+        self.facedown = False
     
     def getNum(self,total):
+        """returns the number on the card"""
         if self.num == 1:
             if total <= 21:
                 return 11
@@ -13,4 +15,13 @@ class Card:
             return self.num
     
     def getID(self):
+        """Example id: c1"""
         return self.id
+
+    def setSide(self,back):
+        """back is a boolean value. True means the card is facing down"""
+        self.facedown = back 
+
+    def getSide(self):
+        """returns True if the card is facing down"""
+        return self.facedown
